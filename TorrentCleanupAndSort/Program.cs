78639080c9643f4366738a3254d61d28234e0946
Mike <mikeundead@googlemail.com>
2018-06-13 20:12:00 +0200

    init

---
+++ TorrentCleanupAndSort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Reflection;
+using TorrentCleanupDLL;
+using TorrentSortDLL;
+
+namespace TorrentCleanupAndSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Cleanup cleanup = new Cleanup();
+            cleanup.Clean();
+
+            Sorter sort = new Sorter(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            sort.Sort();
+        }
+    }
+}
